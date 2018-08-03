373a9d9764207c880b21bd0a35183542f4360385
Sergey97 <Sergey97-7@yandex.ru>
2018-08-03 18:37:25 +0400

    first commit

---
+++ src/components/Article.js
@@ -0,0 +1,50 @@
+import	React, {Component} from 'react'
+
+class Article extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			isOpen: props.defaultOpen
+		}
+	}
+	componentWillMount() {
+		console.log('---', 'mounting')
+	}
+	/*state = {
+		isOpen: true
+	}*/
+	render () {
+		const {article} = this.props
+		console.log('---', this.props)
+		const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+		return (
+			<div className ="card mx-auto" style = {{width: '50%'}}>
+				<div className ="card-header">
+					<h2>
+						{article.title}
+						<button onClick = {this.handleClick} className="btn btn-primary btn-lg float-right">
+						{this.state.isOpen ? 'close' : 'open'}
+						</button>
+					</h2>
+				</div>
+				<div className="card-header"style = {{backgroundColor: 'white'}}>
+					<h6 className="card-subtitle text-muted">
+						creation date: {(new Date(article.date)).toDateString()}
+					</h6>
+					{body}
+				</div>
+			</div>
+		)
+	}
+	handleClick = () => {
+		console.log('---', 'clicked')
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+}
+
+
+export default Article
+// className='hello' style={{color: 'red'}}
